2484bde7789df439f41350c95a95bf5ec8f9b28d
saiviswateja <saiviswateja.teja@gmail.com>
2022-10-22 09:55:19 +0530

    added resilience4j

--- accounts/src/main/java/com/viswa/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/viswa/accounts/controller/AccountsController.java
@@ -5,10 +5,14 @@ import com.viswa.accounts.client.LoansFeignClient;
 import com.viswa.accounts.config.AccountsServiceConfig;
 import com.viswa.accounts.model.*;
 import com.viswa.accounts.repository.AccountRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -50,7 +54,20 @@ public class AccountsController {
         return jsonStr;
     }
 
+    /**
+     * @CircuitBreaker
+     * CircuitBreaker will not retry but directly use tje fall back method
+     *
+     * @Retry
+     * retry will first retry for the count given in application properties and then it will make use of fall back method
+     *
+     *
+     * @param customer
+     * @return
+     */
     @PostMapping("/myCustomerDetails")
+    @CircuitBreaker(name = "detailsForCustomerSupportApp", fallbackMethod = "myCustmerDetailsFallback")
+//    @Retry(name = "detailsForCustomerSupportApp", fallbackMethod = "myCustmerDetailsFallback")
     public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
         Accounts accounts = accountRepository.findByCustomerId(customer.getCustomerId());
         List<Loans> loansList = loansFeignClient.getLoanDetails(customer);
@@ -59,6 +76,29 @@ public class AccountsController {
         CustomerDetails customerDetails = new CustomerDetails();
         customerDetails.setAccounts(accounts);
         customerDetails.setCards(cardsList);
+        customerDetails.setLoans(loansList);
+
+        return customerDetails;
+    }
+
+    @GetMapping("/sayHello")
+    @RateLimiter(name = "sayHello", fallbackMethod = "sayHelloFallback")
+    public String sayHello() {
+        return "Hello, Welcome to Eazy bank";
+    }
+
+    public String sayHelloFallback(Throwable t) {
+        return "Hello, welcome to eazy bank from fall back method";
+    }
+
+    private CustomerDetails myCustmerDetailsFallback(Customer customer, Throwable t) {
+        Accounts accounts = accountRepository.findByCustomerId(customer.getCustomerId());
+        List<Loans> loansList = loansFeignClient.getLoanDetails(customer);
+//        List<Cards> cardsList = cardsFeignClient.getCardDetails(customer);
+
+        CustomerDetails customerDetails = new CustomerDetails();
+        customerDetails.setAccounts(accounts);
+//        customerDetails.setCards(cardsList);
         customerDetails.setLoans(loansList);
 
         return customerDetails;
